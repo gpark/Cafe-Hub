8db8687d693a71491b09481db29c6509a67fc9dc
Aaron Ching <aaronching9001@gmail.com>
2016-10-27 21:05:11 +0000

    fixed buggy test and added coverage to gitignore

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -13,14 +13,14 @@ Given /^"([^"]*)" has preferences/ do |preferences_table|
 end
 
 Then /^I should see "([^"]*)" in the time slot for "([^"]*)" to "([^"]*)" on "([^"]*)"$/ do |entry_name, start_time, end_time, day|
-  mapping = {"Monday" => "day wday-1 past current-month", 
-             "Tuesday" => "day wday-2 past current-month", 
-             "Wednesday" => "day wday-3 past current-month",
-             "Thursday" => "day wday-4 past current-month",
-             "Friday" => "day wday-5 past current-month",
-             "Saturday"=>"day wday-6 past current-month",
-             "Sunday" => "day wday-0 past current-month"}
-  within("//td[@class='" + mapping[day.to_s] + "']") do 
+  mapping = {"Monday" => "day wday-1", 
+             "Tuesday" => "day wday-2", 
+             "Wednesday" => "day wday-3",
+             "Thursday" => "day wday-4",
+             "Friday" => "day wday-5",
+             "Saturday"=>"day wday-6",
+             "Sunday" => "day wday-0"}
+  within("//td[@class^='" + mapping[day.to_s] + "']") do 
       page.should have_content(entry_name)
       page.should have_content(start_time)
       page.should have_content(end_time)
