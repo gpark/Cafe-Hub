25b3548660adb7be429ebd9bd0d9afafd6dd9884
Aaron Ching <aaronching9001@gmail.com>
2016-11-01 21:29:17 +0000

    sign in code hardcoded to 1234

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  attr_accessor :sign_up_code
+  validates :sign_up_code,
+    on: :create,
+    presence: true,
+    inclusion: { in: ["1234"] }
 end
