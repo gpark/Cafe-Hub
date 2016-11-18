182cb69cefa096b95dbb915872cfed2694c4574d
Victor Lu <victorhlu13@gmail.com>
2016-11-18 11:53:33 +0000

    heavy refactoring + more features to algorithm

--- app/models/assignments_week.rb
+++ app/models/assignments_week.rb
@@ -1,10 +1,16 @@
 class AssignmentsWeek < ActiveRecord::Base
     has_many :assignments
     
+    #TODO LIST
+        #Write Tests
+        #Write code for loop around to next day times
+        #Prevent overload to workers in terms of hours or not enough (maybe weighted selection based)
+        #Include blanks and R/N Work hours to fill
     def to_s
         return self.start_date.to_s + " to " + self.end_date.to_s
     end
-    
+
+    #Sets time from 12 hour clock to 24 hour clock, used to fill in hash table from start to end time
     def twelve_to_twentyfour(ampm, num)
         if ampm == "P"
             if num == "12"
@@ -21,7 +27,7 @@ class AssignmentsWeek < ActiveRecord::Base
         end
     end
     
-    
+    #Switches twenty four hour clock cycle to 12 hour clock cycle in string form
     def twentyfour_to_twelve_form(twentyfour_form)
         if twentyfour_form == 0
             return "12:00 AM"
@@ -34,10 +40,8 @@ class AssignmentsWeek < ActiveRecord::Base
         end
     end
     
-    
-    def user_schedule_helper(start_num, start_ampm, end_num, end_ampm, item, su, m, tu, w, th, f, sa, pref_table)
-        start_test = twelve_to_twentyfour(start_ampm, start_num)
-        end_test = twelve_to_twentyfour(end_ampm, end_num)
+    #Fills in hash table with the user's id if they prefer those times to eventually be selected
+    def user_schedule_helper(start_test, end_test, item, su, m, tu, w, th, f, sa, pref_table) 
     	while start_test < end_test
     		if su
     			pref_table["su"][start_test].push(item)
@@ -65,8 +69,7 @@ class AssignmentsWeek < ActiveRecord::Base
     	return pref_table
     end
     
-
-    
+    #Method that is called to create assignments for assignments week
     def generate_assignments
         @users = User.all
         @facilities = Facility.all
@@ -81,207 +84,68 @@ class AssignmentsWeek < ActiveRecord::Base
         		20 => [], 21 => [], 22 => [], 23 => []}
         end
         
+        #ASSIGNMENTS & HASH TABLE OF USER PREFERENCES
         if @users.nil?
             return
         else
             @users.each do |user|
                 pref = user.preferences.order(created_at: :desc).first 
-                if !pref.nil?
-                    for pref_entry in pref.preference_entries.all
-                        type = pref_entry.preference_type
-                        if type == "Prefer"
-                            for entry_occurence in pref_entry.occurences.all
-                                user_id = user.id
-                                start_match = /(.*):00 (.*)M/.match(entry_occurence.start_time) #<MatchData "3:00 AM" 1:"3" 2:"A">
-                                end_match = /(.*):00 (.*)M/.match(entry_occurence.end_time)
-                                available_ppl = user_schedule_helper(start_match[1], start_match[2], end_match[1], end_match[2], user_id, 
-                                    entry_occurence.su, entry_occurence.m, entry_occurence.tu, entry_occurence.w, entry_occurence.th, 
-                                    entry_occurence.f, entry_occurence.sa, available_ppl)
-                            end
+                for pref_entry in pref.preference_entries.all
+                    type = pref_entry.preference_type
+                    if type == "Prefer"
+                        for entry_occurence in pref_entry.occurences.all
+                            user_id = user.id
+                            start_match = /(.*):00 (.*)M/.match(entry_occurence.start_time) #<MatchData "3:00 AM" 1:"3" 2:"A">
+                            end_match = /(.*):00 (.*)M/.match(entry_occurence.end_time)
+                            starting = twelve_to_twentyfour(start_match[2], start_match[1])
+                            ending = twelve_to_twentyfour(end_match[2], end_match[1])
+                            available_ppl = user_schedule_helper(starting, ending, user_id, 
+                                entry_occurence.su, entry_occurence.m, entry_occurence.tu, entry_occurence.w, entry_occurence.th, 
+                                entry_occurence.f, entry_occurence.sa, available_ppl)
                         end
                     end
                 end
             end
         end  
+        
+        #Fill facilities people needs with availabilities
         if @facilities.nil?
             return
         else
             @facilities.each do |facility|
                 needed_num = facility.ppl_per_shift
-                start_match = /(.*):00 (.*)M/.match(facility.m_start)
-                end_match = /(.*):00 (.*)M/.match(facility.m_end)                
-                if not start_match.nil?
-                    start_test = twelve_to_twentyfour(start_match[2], start_match[1])
-                    end_test = twelve_to_twentyfour(end_match[2], end_match[1])
-                	while start_test < end_test
-                	    for i in 1..needed_num
-                	        if not available_ppl["m"][start_test].empty?
-                	            temp_ppl_id = available_ppl["m"][start_test][0]
-                	            temp_db_entry = Assignment.new
-                	            temp_db_entry.user_id = temp_ppl_id
-                	            temp_db_entry.facility_id = facility.id
-                	            temp_db_entry.assignments_week_id = self.id
-                	            temp_db_entry.day = 'm'
-                	            temp_db_entry.start_time = twentyfour_to_twelve_form(start_test)
-                	            temp_db_entry.end_time = twentyfour_to_twelve_form(start_test + 1)
-                	            temp_db_entry.save
-                	            available_ppl["m"][start_test].delete_at(0)
-                	        end
-                	    end
-                	    start_test += 1
-                	end
+                counter = 0
+                storage = {0 => ['m', facility.m_start, facility.m_end], 1 => ['tu', facility.tu_start, facility.tu_end], 
+                            2 => ['w', facility.w_start, facility.w_end], 3 => ['th', facility.th_start, facility.th_end],
+        		            4 => ['f', facility.f_start, facility.f_end], 5 => ['sa', facility.sa_start, facility.sa_end], 
+        		            6 => ['su', facility.su_start, facility.su_end]}
+                while counter < 7
+                    if not storage[counter][1].nil?
+                        start_match = /(.*):00 (.*)M/.match(storage[counter][1])
+                        end_match = /(.*):00 (.*)M/.match(storage[counter][2])
+                        start_test = twelve_to_twentyfour(start_match[2], start_match[1])
+                        end_test = twelve_to_twentyfour(end_match[2], end_match[1])
+                    	while start_test < end_test
+                    	    ppl = available_ppl[storage[counter][0]][start_test]
+                    	    if not ppl.empty?
+                    	        chosen = ([ppl.size, needed_num].min).times.map{Random.rand(ppl.size)}
+                    	        for index in chosen
+                    	            db_entry = Assignment.new
+                    	            db_entry.user_id = ppl[index]
+                    	            db_entry.facility_id = facility.id
+                    	            db_entry.assignments_week_id = self.id
+                    	            db_entry.day = storage[counter][0]
+                    	            db_entry.start_time = twentyfour_to_twelve_form(start_test)
+                    	            db_entry.end_time = twentyfour_to_twelve_form(start_test + 1)
+                    	            db_entry.save
+                    	            start_test += 1
+                    	        end
+                    	    end
+                    	end
+                    end
+                    counter += 1
                 end
-                
-                start_match = /(.*):00 (.*)M/.match(facility.tu_start)
-                end_match = /(.*):00 (.*)M/.match(facility.tu_end)    
-                if not start_match.nil?
-                    start_test = twelve_to_twentyfour(start_match[2], start_match[1])
-                    end_test = twelve_to_twentyfour(end_match[2], end_match[1])
-                	
-                	while start_test < end_test
-                	    for i in 1..needed_num
-                	        if not available_ppl["tu"][start_test].empty?
-                	            temp_ppl_id = available_ppl["tu"][start_test][0]
-                	            temp_db_entry = Assignment.new
-                	            temp_db_entry.user_id = temp_ppl_id
-                	            temp_db_entry.facility_id = facility.id
-                	            temp_db_entry.assignments_week_id = self.id
-                	            temp_db_entry.day = 'tu'
-                	            temp_db_entry.start_time = twentyfour_to_twelve_form(start_test)
-                	            temp_db_entry.end_time = twentyfour_to_twelve_form(start_test + 1)
-                	            temp_db_entry.save
-                	            available_ppl["tu"][start_test].delete_at(0)
-                	        end
-                	    end
-                	    start_test += 1
-                	end
-                end
-                start_match = /(.*):00 (.*)M/.match(facility.w_start)
-                end_match = /(.*):00 (.*)M/.match(facility.w_end)                
-                if not start_match.nil?
-                    start_test = twelve_to_twentyfour(start_match[2], start_match[1])
-                    end_test = twelve_to_twentyfour(end_match[2], end_match[1])
-                	while start_test < end_test
-                	    for i in 1..needed_num
-                	        if not available_ppl["w"][start_test].empty?
-                	            temp_ppl_id = available_ppl["w"][start_test][0]
-                	            temp_db_entry = Assignment.new
-                	            temp_db_entry.user_id = temp_ppl_id
-                	            temp_db_entry.facility_id = facility.id
-                	            temp_db_entry.assignments_week_id = self.id
-                	            temp_db_entry.day = 'w'
-                	            temp_db_entry.start_time = twentyfour_to_twelve_form(start_test)
-                	            temp_db_entry.end_time = twentyfour_to_twelve_form(start_test + 1)
-                	            temp_db_entry.save
-                	            available_ppl["w"][start_test].delete_at(0)
-                	        end
-                	    end
-                	    start_test += 1
-                	end
-                end
-
-                start_match = /(.*):00 (.*)M/.match(facility.th_start)
-                end_match = /(.*):00 (.*)M/.match(facility.th_end)                
-                if not start_match.nil?
-                    start_test = twelve_to_twentyfour(start_match[2], start_match[1])
-                    end_test = twelve_to_twentyfour(end_match[2], end_match[1])
-                	
-                	while start_test < end_test
-                	    for i in 1..needed_num
-                	        if not available_ppl["th"][start_test].empty?
-                	            temp_ppl_id = available_ppl["th"][start_test][0]
-                	            temp_db_entry = Assignment.new
-                	            temp_db_entry.user_id = temp_ppl_id
-                	            temp_db_entry.facility_id = facility.id
-                	            temp_db_entry.assignments_week_id = self.id
-                	            temp_db_entry.day = 'th'
-                	            temp_db_entry.start_time = twentyfour_to_twelve_form(start_test)
-                	            temp_db_entry.end_time = twentyfour_to_twelve_form(start_test + 1)
-                	            temp_db_entry.save
-                	            available_ppl["th"][start_test].delete_at(0)
-                	        end
-                	    end
-                	    start_test += 1
-                	end
-                end
-                
-                start_match = /(.*):00 (.*)M/.match(facility.f_start)
-                end_match = /(.*):00 (.*)M/.match(facility.f_end)                
-                if not start_match.nil?
-                    start_test = twelve_to_twentyfour(start_match[2], start_match[1])
-                    end_test = twelve_to_twentyfour(end_match[2], end_match[1])
-                	
-                	while start_test < end_test
-                	    for i in 1..needed_num
-                	        if not available_ppl["f"][start_test].empty?
-                	            temp_ppl_id = available_ppl["f"][start_test][0]
-                	            temp_db_entry = Assignment.new
-                	            temp_db_entry.user_id = temp_ppl_id
-                	            temp_db_entry.facility_id = facility.id
-                	            temp_db_entry.assignments_week_id = self.id
-                	            temp_db_entry.day = 'f'
-                	            temp_db_entry.start_time = twentyfour_to_twelve_form(start_test)
-                	            temp_db_entry.end_time = twentyfour_to_twelve_form(start_test + 1)
-                	            temp_db_entry.save
-                	            available_ppl["f"][start_test].delete_at(0)
-                	        end
-                	    end
-                	    start_test += 1
-                	end
-                end
-                
-                start_match = /(.*):00 (.*)M/.match(facility.sa_start)
-                end_match = /(.*):00 (.*)M/.match(facility.sa_end)
-                if not start_match.nil?
-                    start_test = twelve_to_twentyfour(start_match[2], start_match[1])
-                    end_test = twelve_to_twentyfour(end_match[2], end_match[1])
-                	
-                	while start_test < end_test
-                	    for i in 1..needed_num
-                	        if not available_ppl["sa"][start_test].empty?
-                	            temp_ppl_id = available_ppl["sa"][start_test][0]
-                	            temp_db_entry = Assignment.new
-                	            temp_db_entry.user_id = temp_ppl_id
-                	            temp_db_entry.facility_id = facility.id
-                	            temp_db_entry.assignments_week_id = self.id
-                	            temp_db_entry.day = 'sa'
-                	            temp_db_entry.start_time = twentyfour_to_twelve_form(start_test)
-                	            temp_db_entry.end_time = twentyfour_to_twelve_form(start_test + 1)
-                	            temp_db_entry.save
-                	            available_ppl["sa"][start_test].delete_at(0)
-                	        end
-                	    end
-                	    start_test += 1
-                	end
-                end
-
-                start_match = /(.*):00 (.*)M/.match(facility.su_start)
-                end_match = /(.*):00 (.*)M/.match(facility.su_end)                
-                if not start_match.nil?
-                    start_test = twelve_to_twentyfour(start_match[2], start_match[1])
-                    end_test = twelve_to_twentyfour(end_match[2], end_match[1])
-                	
-                	while start_test < end_test
-                	    for i in 1..needed_num
-                	        if not available_ppl["su"][start_test].empty?
-                	            temp_ppl_id = available_ppl["su"][start_test][0]
-                	            temp_db_entry = Assignment.new
-                	            temp_db_entry.user_id = temp_ppl_id
-                	            temp_db_entry.facility_id = facility.id
-                	            temp_db_entry.assignments_week_id = self.id
-                	            temp_db_entry.day = 'su'
-                	            temp_db_entry.start_time = twentyfour_to_twelve_form(start_test)
-                	            temp_db_entry.end_time = twentyfour_to_twelve_form(start_test + 1)
-                	            temp_db_entry.save
-                	            available_ppl["su"][start_test].delete_at(0)
-                	        end
-                	    end
-                	    start_test += 1
-                	end
-                end
-                
             end
         end
     end
-
 end
