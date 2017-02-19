7cb1937ed49640ec40c13c74bac34c29be0dca54
Grace Park <gpark@berkeley.edu>
2017-02-19 15:41:42 -0800

    started making new functions for refactoring

--- lib/patches/fixnum.rb
+++ lib/patches/fixnum.rb
@@ -12,5 +12,29 @@ class Fixnum
             return (self - 12).to_s + ":00 PM"
         end
     end
-    
+
+    def to_timestring
+      if self > 336
+          raise 'This method is not applicable for this number'
+      end
+      # // 48 to determine day (Sunday = 0, Saturday = 6)
+      # % 48 to determine time
+      day_of_week = ["Su", "M", "Tu", "W", "Th", "F", "Sa"]
+      day_str = day_of_week[(self / 48).floor]
+      time = ((self % 48) / 2).floor
+      hr = ":00"
+      if self % 2 != 0 # on the half hour
+        hr = ":30"
+      end
+      ampm = " AM"
+      if time >= 12 # AM
+        ampm = " PM"
+        time = time - 12
+      end
+      if time == 0
+        time = 12
+      end
+      return day_str + " " + time.to_s + hr + ampm
+    end
+
 end
--- lib/patches/string.rb
+++ lib/patches/string.rb
@@ -19,6 +19,28 @@ class String
                 return num.to_i
             end
         end
-    end    
-    
+    end
+
+    def to_hr_form
+        matches = /(.*) (.*):(.*) (.*)M/.match(self) #<MatchData "M 3:30 AM" 1:"M" 2:"3" 3:"30" 4:"A">
+        if matches == nil
+            raise 'This method is not applicable for the string: ' + self
+        end
+        day = ["Su", "M", "Tu", "W", "Th", "F", "Sa"].index(matches[1])
+        hr = matches[2]
+        min = matches[3]
+        ampm = matches[4]
+        if hr == "12"
+            hr = "0"
+        end
+        total = (day * 48) + hr.to_i * 2
+        if min == "30"
+            total += 1
+        end
+        if ampm == "P"
+            total += 24
+        end
+        return total
+    end
+
 end
